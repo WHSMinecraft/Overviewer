1825da248153c5e02d343dac1653fafb561eeef2
Ruben Ollesch <ruben.ollesch@studmail.w-hs.de>
2020-12-02 16:16:40 +0100

    Fix whitespace & crop/zoom settings

--- config.py
+++ config.py
@@ -9,74 +9,84 @@ worlds["Nether"] = folder + "world_nether"
 worlds["End"] = folder + "world_the_end"
 
 
-radius = 100
-center = (-600, 70, -200)
-border = (center[0]-radius, center[2]-radius, center[0]+radius, center[2]+radius)
-
+radius = 50000
 
 renders["overworldday"] = {
-            "title": "Welt am Tag",
-                "world": "Overworld",
-                    "rendermode": smooth_lighting,
-                        "crop": border,
-                            "center": center,
-                                "showspawn": False
-                                }
+	"title": "Welt am Tag",
+	"world": "Overworld",
+	"rendermode": smooth_lighting,
+	"crop": (-radius, -radius, radius, radius),
+	"center": (-600, 80, -180),
+	"showspawn": False,
+        "defaultzoom": 5
+}
 
+"""
 renders["overworldnight"] = {
-            "title": "Welt in der Nacht",
-                "world": "Overworld",
-                    "rendermode": [Base(), EdgeLines(), SmoothLighting(night=True, strength=0.7)],
-                        "crop": border,
-                            "center": center,
-                                "showspawn": False
-                                }
+	"title": "Welt in der Nacht",
+	"world": "Overworld",
+	"rendermode": [Base(), EdgeLines(), SmoothLighting(night=True, strength=0.7)],
+	"crop": border,
+	"center": center,
+	"showspawn": False
+}
+"""
 
+"""
 renders['overworldcaves'] = {
-            'title': "Unter Tage",
-                'world': 'Overworld',
-                    'rendermode': [Base(), DepthTinting(), Cave(only_lit=True)],
-                        "crop": border
-                        }
+	'title': "Unter Tage",
+	'world': 'Overworld',
+	'rendermode': [Base(), DepthTinting(), Cave(only_lit=True)],
+	"crop": border
+}
+"""
 
 renders['netherhighways'] = {
-            "title": 'Highways',
-                'world': 'Nether',
-                    "rendermode": [Base(), Depth(min=0, max=53), EdgeLines(), Nether(), SmoothLighting(strength=0.4)],
-                        "northdirection": "lower-right"
-                        }
+	"title": 'Highways',
+	'world': 'Nether',
+	"rendermode": [Base(), Depth(min=0, max=53), EdgeLines(), Nether(), SmoothLighting(strength=0.4)],
+	"northdirection": "lower-right",
+        "defaultzoom": 8,
+        "center": (-250, 50, 0)
+}
 
 
 renders['netheritemines'] = {
-            "title": 'Netherite Minen',
-                'world': 'Nether',
-                    "rendermode": [Base(), Depth(min=0, max=14), EdgeLines(), Nether(), SmoothLighting(strength=0.4)],
-                        "northdirection": "lower-right"
-                        }
+	"title": 'Netherite Minen',
+	'world': 'Nether',
+	"rendermode": [Base(), Depth(min=0, max=15), EdgeLines(), Nether(), SmoothLighting(strength=0.4)],
+	"northdirection": "lower-right",
+        "defaultzoom": 8,
+        "center": (-250, 50, 0)
+}
 
 renders['end'] = {
-            "title": 'Das Ende',
-                'world': 'End',
-                    "rendermode": [Base(), EdgeLines(), SmoothLighting(strength=0.3)]
-                    }
+	"title": 'Das Ende',
+	'world': 'End',
+	"rendermode": [Base(), EdgeLines(), SmoothLighting(strength=0.3)],
+        "defaultzoom": 8,
+        "center": (0, 80, 0)
+}
 
+"""
 
 renders['slimeoverlay'] = {
-            'title': "Slimechunks",
-                'world': 'Overworld',
-                    'rendermode': [ClearBase(), SlimeOverlay()],
-                        "crop": border,
-                            'overlay': ['overworldday', 'overworldnight', 'overworldcaves']
-                            }
+	'title': "Slimechunks",
+	'world': 'Overworld',
+	'rendermode': [ClearBase(), SlimeOverlay()],
+	"crop": border,
+	'overlay': ['overworldday', 'overworldnight', 'overworldcaves']
+}
 
 
 
 renders['biomeoverlay'] = {
-            'title': "Biome",
-                'world': 'Overworld',
-                    'rendermode': [ClearBase(), BiomeOverlay()],
-                        "crop": border,
-                            'overlay': ['overworldday', 'overworldnight', 'overworldcaves']
-                            }
+	'title': "Biome",
+	'world': 'Overworld',
+	'rendermode': [ClearBase(), BiomeOverlay()],
+	"crop": border,
+	'overlay': ['overworldday', 'overworldnight', 'overworldcaves']
+}
 
 
+"""
