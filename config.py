3b34d2d84df683649dd2ab7feea87a2d7599ef85
Ruben Ollesch <ruben.ollesch@protonmail.com>
2021-01-10 15:03:06 +0100

    Refactor and clean up render config

--- config.py
+++ config.py
@@ -35,98 +35,78 @@ def markerFilter(poi):
                 pages = map(getJSONText, nbt['pages'][1:])  # Rest of pages
                 return (title, " ".join(pages))
 
+
 markers = [dict(name="Marker", filterFunction=markerFilter, icon="signpost_icon.png", showIconInLegend=True)]
 
-radius = 50000
+
+# ==================== Overworld =====================
+center = (0, 80, 0)
+radius = 20000
+crop = (center[0]-radius, center[2]-radius, center[0]+radius, center[2]+radius)
 
 renders["overworldday"] = {
-	"title": "Welt am Tag",
+	"title": "Tag",
 	"world": "Overworld",
 	"rendermode": smooth_lighting,
-	"crop": (-radius, -radius, radius, radius),
-	"center": (-600, 80, -180),
+	"crop": crop,
+	"center": center,
 	"showspawn": False,
         "defaultzoom": 5,
+        "maxzoom": -1,
         "markers": markers
 }
 
-
-renders["overworldnight"] = {
-	"title": "Welt in der Nacht",
-	"world": "Overworld",
-	"rendermode": [Base(), EdgeLines(), SmoothLighting(night=True, strength=0.7)],
-	"crop": (-radius, -radius, radius, radius),
-	"center": (-600, 80, -180),
-	"showspawn": False,
-        "defaultzoom": 5,
-        "markers": markers
-}
-
-"""
-renders['overworldcaves'] = {
-	'title': "Unter Tage",
-	'world': 'Overworld',
-	'rendermode': [Base(), DepthTinting(), Cave(only_lit=True)],
-	"crop": border
-}
-"""
-
-renders['netherceiling'] = {
-	"title": 'Über Bedrock',
-	'world': 'Nether',
-	"rendermode": [Base(), EdgeLines(), SmoothLighting(strength=0.4)],
-        "defaultzoom": 8,
-        "center": (-250, 50, 0),
-        "markers": markers
-}
+# ==================== Nether =====================
+radius = 1000
+center = (0, 80, 0)
+crop = (center[0]-radius, center[2]-radius, center[0]+radius, center[2]+radius)
 
 renders['netherhighways'] = {
 	"title": 'Highways',
 	'world': 'Nether',
 	"rendermode": [Base(), Depth(min=0, max=53), EdgeLines(), Nether(), SmoothLighting(strength=0.4)],
         "defaultzoom": 8,
-        "center": (-250, 50, 0),
+        "maxzoom": -1,
+        "center": center,
+        "crop": crop,
         "markers": markers
 }
 
 
+renders['netherceiling'] = {
+	"title": 'Über Bedrock',
+	'world': 'Nether',
+	"rendermode": [Base(), EdgeLines(), SmoothLighting(strength=0.4)],
+        "defaultzoom": 8,
+        "maxzoom": -1,
+        "center": center,
+        "crop": crop,
+        "markers": markers
+}
 renders['netheritemines'] = {
 	"title": 'Netherite Minen',
 	'world': 'Nether',
 	"rendermode": [Base(), Depth(min=0, max=15), EdgeLines(), Nether(), SmoothLighting(strength=0.4)],
         "defaultzoom": 8,
-        "center": (-250, 50, 0),
+        "maxzoom": -1,
+        "center": center,
+        "crop": crop,
         "markers": markers
 }
+
+
+# ==================== End =====================
+radius = 30000
+center = (0, 80, 0)
+crop = (center[0]-radius, center[2]-radius, center[0]+radius, center[2]+radius)
 
 renders['end'] = {
 	"title": 'Das Ende',
 	'world': 'End',
 	"rendermode": [Base(), EdgeLines(), SmoothLighting(strength=0.3)],
-        "defaultzoom": 8,
-        "center": (0, 80, 0),
+        "defaultzoom": 6,
+        "maxzoom": -1,
+        "center": center,
+        "crop": crop,
         "markers": markers
 }
-
-"""
-
-renders['slimeoverlay'] = {
-	'title': "Slimechunks",
-	'world': 'Overworld',
-	'rendermode': [ClearBase(), SlimeOverlay()],
-	"crop": border,
-	'overlay': ['overworldday', 'overworldnight', 'overworldcaves']
-}
-
-
-
-renders['biomeoverlay'] = {
-	'title': "Biome",
-	'world': 'Overworld',
-	'rendermode': [ClearBase(), BiomeOverlay()],
-	"crop": border,
-	'overlay': ['overworldday', 'overworldnight', 'overworldcaves']
-}
-
-
-"""
